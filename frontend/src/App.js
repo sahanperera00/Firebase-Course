4114d6ff0a9e141191e6610ef1a526a042cc001c
Sahan Perera <sahanvimukthi00@gmail.com>
2022-12-03 18:13:37 +0530

    did some changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,98 +1,13 @@
 import "./index.css";
-import { useState } from "react";
-import { app, databese } from "./firebaseConfig";
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut,
-  deleteUser,
-  signInWithPopup,
-  GoogleAuthProvider,
-} from "firebase/auth";
-import { collection, addDoc } from "firebase/firestore";
+import Authentication from "./components/Authentication";
+import FirestoreDatabase from "./components/FirestoreDatabase";
 
 function App() {
-  const auth = getAuth();
-  let googleProvider = new GoogleAuthProvider();
-  const [data, setData] = useState({});
-
-  const handleInput = (event) => {
-    let newInput = { [event.target.name]: event.target.value };
-    setData({ ...data, ...newInput });
-  };
-
-  const handleSignup = () => {
-    createUserWithEmailAndPassword(auth, data.email, data.password)
-      .then((response) => {
-        handleSignout();
-        alert("Signup Successfull");
-      })
-      .catch((error) => {
-        alert(error.message);
-      });
-  };
-
-  const handleSignin = () => {
-    let user = auth.currentUser;
-    if (user == null) {
-      signInWithEmailAndPassword(auth, data.email, data.password)
-        .then(() => {
-          alert("Signin Successfull");
-        })
-        .catch((error) => {
-          alert("Signin Unsuccessfull");
-        });
-    } else {
-      alert("Already Signed in");
-    }
-  };
-
-  const handleSignout = () => {
-    let user = auth.currentUser;
-    if (user != null) {
-      signOut(auth)
-        .then(() => {
-          alert("Signout Successfull");
-        })
-        .catch((error) => {
-          alert("Signout Unsuccessfull");
-        });
-    } else {
-      alert("Already Signed Out");
-    }
-  };
-
-  const handleUserAccountDelete = () => {
-    let user = auth.currentUser;
-    deleteUser(user)
-      .then(() => {
-        alert("User Account Deleted");
-      })
-      .catch((error) => {
-        alert(error.message);
-      });
-  };
-
   return (
     <div className="App">
       <h3>Firebase Basics</h3>
-      <input
-        name="email"
-        placeholder="Email"
-        onChange={(event) => handleInput(event)}
-      />
-      <input
-        name="password"
-        placeholder="Password"
-        onChange={(event) => handleInput(event)}
-      />
-      <br />
-      <h3>Authenticate with Firebase using Password-Based Accounts</h3>
-      <button onClick={handleSignup}>Create Account</button>
-      <button onClick={handleSignin}>Sign in</button>
-      <button onClick={handleSignout}>Sign out</button>
-      <button onClick={handleUserAccountDelete}>Delete Account</button>
+      <Authentication />
+      <FirestoreDatabase />
     </div>
   );
 }
