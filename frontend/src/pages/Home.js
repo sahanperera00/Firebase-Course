c1f288030eb9e3fce05e05e9bad49023e69b6518
Sahan Perera <sahanvimukthi00@gmail.com>
2022-12-09 18:03:53 +0530

    did some changes

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,36 @@
+import { useEffect } from "react";
+import { getAuth, signOut, onAuthStateChanged } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { app } from "../firebaseConfig";
+
 function Home() {
+  const auth = getAuth(app);
+  let user = auth.currentUser;
+  const navigate = useNavigate();
+
+  const handleSignout = () => {
+    signOut(auth)
+      .then(() => {
+        //   alert("Signout Successfull");
+        navigate("/login");
+      })
+      .catch((error) => {
+        alert("Signout Unsuccessfull");
+      });
+  };
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        navigate("/login");
+      }
+    });
+  }, []);
+
   return (
     <>
       <h1>Home</h1>
+      <button onClick={handleSignout}>Sign out</button>
     </>
   );
 }
