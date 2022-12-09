3170627723db9b4f7cdb12c162160c00d08abef3
Sahan Perera <sahanvimukthi00@gmail.com>
2022-12-10 01:46:25 +0530

    signup implemented

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,7 +1,65 @@
+import { useState } from "react";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+
 function Signup() {
+  const [data, setData] = useState({});
+  const auth = getAuth();
+
+  const handleInput = (event) => {
+    let newInput = { [event.target.name]: event.target.value };
+    setData({ ...data, ...newInput });
+  };
+
+  const handleSignout = () => {
+    let user = auth.currentUser;
+    if (user != null) {
+      signOut(auth)
+        .then(() => {
+          alert("Signout Successfull");
+        })
+        .catch((error) => {
+          alert("Signout Unsuccessfull");
+        });
+    } else {
+      alert("Already Signed Out");
+    }
+  };
+
+  const handleSignup = () => {
+    if (data.email != null && data.password != null) {
+      createUserWithEmailAndPassword(auth, data.email, data.password)
+        .then((response) => {
+          handleSignout();
+          alert("Signup Successfull");
+        })
+        .catch((error) => {
+          alert(error.message);
+        });
+    } else {
+      alert("Please fill all the fields");
+    }
+  };
+
   return (
     <>
       <h1>Signup</h1>
+      <input
+        name="email"
+        placeholder="Email"
+        onChange={(event) => handleInput(event)}
+      />
+      <br />
+      <input
+        name="password"
+        placeholder="Password"
+        onChange={(event) => handleInput(event)}
+      />
+      <br />
+      <button onClick={handleSignup}>Create Account</button>
     </>
   );
 }
