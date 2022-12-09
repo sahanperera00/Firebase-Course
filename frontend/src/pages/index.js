64b67b1c7791c2d13ff8088411afb4712b42e7b4
Sahan Perera <sahanvimukthi00@gmail.com>
2022-12-09 17:17:27 +0530

    index created

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,6 @@
+import Login from "./Login";
+import Signup from "./Signup";
+import HomeSharedLayout from "./HomeSharedLayout";
+import Home from "./Home";
+
+export { Login, Signup, HomeSharedLayout, Home };
