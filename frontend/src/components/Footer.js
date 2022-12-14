5d05af0611efb2bb12654f3081e5f62bfd2b6246
Sahan Perera <sahanvimukthi00@gmail.com>
2022-12-14 21:44:56 +0530

    implemented the footer

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,19 @@
+function Footer() {
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "60px",
+        margin: "0px",
+        backgroundColor: "#FFE080",
+        display: "flex",
+        justifyContent: "center",
+        position: "absolute",
+        bottom: "0",
+        right: "0",
+      }}
+    ></div>
+  );
+}
+
+export default Footer;
