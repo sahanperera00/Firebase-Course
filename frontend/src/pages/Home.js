2dfa19a72bd8c08ac687c41b9e9c8c42d7895b83
Sahan Perera <sahanvimukthi00@gmail.com>
2022-12-16 13:14:30 +0530

    did changes to home

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -5,29 +5,16 @@ import {
   onAuthStateChanged,
   deleteUser,
 } from "firebase/auth";
-import { redirect, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { databese } from "../firebaseConfig";
-import {
-  collection,
-  getDocs,
-  doc,
-  updateDoc,
-  deleteDoc,
-} from "firebase/firestore";
-import ReadDataReal from "./ReadDataReal";
+import { collection, getDocs, doc, deleteDoc } from "firebase/firestore";
 
 function Home() {
-  const [data, setData] = useState({});
   const [array, setArray] = useState([]);
   const collectionRef = collection(databese, "users");
 
   const auth = getAuth();
   const navigate = useNavigate();
-
-  const handleInput = (event) => {
-    let newInput = { [event.target.name]: event.target.value };
-    setData({ ...data, ...newInput });
-  };
 
   const handleGetAllData = () => {
     getDocs(collectionRef)
@@ -39,25 +26,11 @@ function Home() {
       });
   };
 
-  const handleUpdateData = (id) => {
-    const docToUpdate = doc(databese, "users", id);
-    updateDoc(docToUpdate, {
-      password: data.password,
-    })
-      .then(() => {
-        // alert("Data Updated");
-      })
-      .catch((error) => {
-        alert(error.message);
-      });
-  };
-
   const handleDeleteData = (id) => {
     const docToDelete = doc(databese, "users", id);
     deleteDoc(docToDelete)
       .then(() => {
-        // alert("Data Deleted");
-        // navigate("/");
+        alert("Data Deleted");
         window.location.reload();
       })
       .catch((error) => {
@@ -68,7 +41,7 @@ function Home() {
   const handleSignout = async () => {
     await signOut(auth)
       .then(() => {
-        //   alert("Signout Successfull");
+        alert("Signout Successfull");
         navigate("/");
       })
       .catch((error) => {
@@ -80,7 +53,8 @@ function Home() {
     let user = auth.currentUser;
     deleteUser(user)
       .then(() => {
-        // alert("User Account Deleted");
+        alert("User Account Deleted");
+        window.location.reload();
       })
       .catch((error) => {
         alert(error.message);
@@ -101,38 +75,151 @@ function Home() {
 
   return (
     <>
-      <h1>Home</h1>
-      <button onClick={handleSignout}>Sign out</button>
-      <button onClick={handleUserAccountDelete}>Delete Account</button>
-      <button
-        onClick={(e) => {
-          navigate("/add");
+      <br />
+      <div
+        style={{
+          width: "70%",
+          height: "450px",
+          marginLeft: "auto",
+          marginRight: "auto",
+          marginTop: "60px",
+          marginBottom: "60px",
+          paddingTop: "30px",
+          backgroundColor: "#02020280",
         }}
       >
-        Add Data
-      </button>
-      <br />
-      {array.map((item) => {
-        return (
-          <div
-            style={{
-              width: "300px",
-              height: "80px",
-              backgroundColor: "#999999",
-              margin: "10px",
-            }}
-          >
-            <span>{item.data().email}</span>
-            <br />
-            <span>{item.data().password}</span>
-            <br />
-            <button onClick={() => handleUpdateData(item.id)}>Update</button>
-            <button onClick={() => handleDeleteData(item.id)}>Delete</button>
-            <br />
+        <div
+          style={{
+            width: "90%",
+            height: "80%",
+            marginLeft: "auto",
+            marginRight: "auto",
+            overflow: "scroll",
+          }}
+        >
+          <table className="table table-dark table-striped table-hover">
+            <thead>
+              <tr>
+                <th scope="col">Image</th>
+                <th scope="col">Name</th>
+                <th scope="col">Age</th>
+                <th scope="col">Update</th>
+                <th scope="col">Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {array.map((item) => {
+                return (
+                  <tr>
+                    <td>
+                      <img
+                        style={{ width: "50px", height: "50px" }}
+                        src={item.data().image}
+                        alt="img"
+                      />
+                    </td>
+                    <td>{item.data().name}</td>
+                    <td>{item.data().age}</td>
+                    <td>
+                      <a
+                        style={{
+                          cursor: "pointer",
+                        }}
+                        onClick={() => navigate(`/update/${item.id}`)}
+                      >
+                        <span className="material-symbols-outlined">
+                          update
+                        </span>
+                      </a>
+                    </td>
+                    <td>
+                      <a
+                        style={{
+                          cursor: "pointer",
+                        }}
+                        onClick={() => handleDeleteData(item.id)}
+                      >
+                        <span className="material-symbols-outlined">
+                          delete
+                        </span>
+                      </a>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+        <button
+          type="button"
+          className="btn btn-dark"
+          style={{
+            marginTop: "15px",
+            marginBottom: "15px",
+            marginLeft: "5%",
+          }}
+          onClick={(e) => {
+            navigate("/add");
+          }}
+        >
+          Add Data
+        </button>
+
+        <button
+          type="button"
+          className="btn btn-dark"
+          style={{
+            marginTop: "15px",
+            marginBottom: "15px",
+            marginLeft: "10px",
+          }}
+          data-bs-toggle="modal"
+          data-bs-target="#exampleModal"
+        >
+          Profile
+        </button>
+
+        <div
+          className="modal fade"
+          id="exampleModal"
+          tabIndex="-1"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog modal-dialog-centered">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h1 className="modal-title fs-5" id="exampleModalLabel">
+                  Profile
+                </h1>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-primary"
+                  data-bs-dismiss="modal"
+                  onClick={handleSignout}
+                >
+                  Signout
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-danger"
+                  onClick={handleUserAccountDelete}
+                >
+                  Delete Account
+                </button>
+              </div>
+            </div>
           </div>
-        );
-      })}
-      <ReadDataReal />
+        </div>
+      </div>
     </>
   );
 }
