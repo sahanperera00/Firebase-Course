7c2346dadfe5739d404fe890a6314ef3cde677b7
Sahan Perera <sahanvimukthi00@gmail.com>
2022-12-16 13:22:35 +0530

    Revert "Delete firebaseConfig.js"
    This reverts commit dbb2102f54ab2bb15f65429b2c6785e204649eef.

---
+++ frontend/src/firebaseConfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCRSI004wdLhieqfO93whI-x7brfQy4VIo",
+  authDomain: "fir-frontend-c2209.firebaseapp.com",
+  projectId: "fir-frontend-c2209",
+  storageBucket: "fir-frontend-c2209.appspot.com",
+  messagingSenderId: "1066573927401",
+  appId: "1:1066573927401:web:82513b730ace238de1f178",
+};
+
+export const app = initializeApp(firebaseConfig);
+export const databese = getFirestore(app);
+export const storage = getStorage(app);
