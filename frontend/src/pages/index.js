cd277a7f2a921b2a7bfcf500e683c5c2cc7d26ad
Sahan Perera <sahanvimukthi00@gmail.com>
2022-12-16 13:14:44 +0530

    added some components

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -3,6 +3,15 @@ import Signup from "./Signup";
 import HomeSharedLayout from "./HomeSharedLayout";
 import Home from "./Home";
 import AddDataForm from "./AddDataForm";
+import UpdateDataForm from "./UpdateDataForm";
 import ReadDataReal from "./ReadDataReal";
 
-export { Login, Signup, HomeSharedLayout, Home, AddDataForm, ReadDataReal };
+export {
+  Login,
+  Signup,
+  HomeSharedLayout,
+  Home,
+  AddDataForm,
+  ReadDataReal,
+  UpdateDataForm,
+};
