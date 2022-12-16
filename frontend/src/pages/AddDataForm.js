90230c0f6ad753eea02d47be660bbdf8182f94cb
Sahan Perera <sahanvimukthi00@gmail.com>
2022-12-16 13:14:07 +0530

    did changes to add data form

--- frontend/src/pages/AddDataForm.js
+++ frontend/src/pages/AddDataForm.js
@@ -1,60 +1,124 @@
 import { useState } from "react";
 import { collection, addDoc } from "firebase/firestore";
-import { app, databese } from "../firebaseConfig";
+import { databese, storage } from "../firebaseConfig";
 import { useNavigate } from "react-router-dom";
-import { getDatabase, ref, set } from "firebase/database";
-import { getAuth } from "firebase/auth";
+import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
 
 function AddDataForm() {
   const [data, setData] = useState({});
+  const [file, setFile] = useState(null);
   const navigate = useNavigate();
-  // const auth = getAuth();
   const collectionRef = collection(databese, "users");
-
-  // const databese = getDatabase(app);
-  // const dbRef = ref(databese, "/users/");
 
   const handleInput = (event) => {
     let newInput = { [event.target.name]: event.target.value };
     setData({ ...data, ...newInput });
   };
 
-  const handleAddData = () => {
-    if (data.email != null && data.password != null) {
-      addDoc(collectionRef, {
-        email: data.email,
-        password: data.password,
+  const handleAddData = (downloadURL) => {
+    addDoc(collectionRef, {
+      name: data.name,
+      age: data.age,
+      image: downloadURL,
+    })
+      .then(() => {
+        alert("Data Added");
+        navigate("/");
       })
-        .then(() => {
-          //   alert("Data Added");
-          navigate("/");
-        })
-        .catch((error) => {
-          console.log(error.message);
-          alert(error.message);
-        });
+      .catch((error) => {
+        console.log(error.message);
+        alert(error.message);
+      });
+  };
+
+  const handleUpload = () => {
+    if (data.name != null && data.age != null && file != null) {
+      const storageRef = ref(storage, `images/${file.name}`);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+        },
+        (error) => {
+          console.log(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            console.log("File available at", downloadURL);
+            handleAddData(downloadURL);
+          });
+        }
+      );
     } else {
       alert("Please fill all the fields");
     }
   };
 
   return (
-    <>
-      <h1>Add Data</h1>
-      <input
-        name="email"
-        placeholder="Email"
-        onChange={(event) => handleInput(event)}
-      />
-      <br />
-      <input
-        name="password"
-        placeholder="Password"
-        onChange={(event) => handleInput(event)}
-      />
-      <br />
-      <button onClick={handleAddData}>Add Data</button>
-    </>
+    <div
+      style={{
+        width: "350px",
+        backgroundColor: "#FFE080",
+        marginLeft: "auto",
+        marginRight: "auto",
+        marginTop: "60px",
+        marginBottom: "60px",
+      }}
+    >
+      <div
+        style={{
+          width: "70%",
+          marginLeft: "auto",
+          marginRight: "auto",
+          padding: "35px 0px",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+        }}
+      >
+        <h1>Add Data</h1>
+        <div className="mb-3">
+          <label className="form-label">Name</label>
+          <input
+            name="name"
+            type="text"
+            placeholder="Name"
+            onChange={(event) => handleInput(event)}
+            className="form-control"
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Age</label>
+          <input
+            name="age"
+            type="number"
+            placeholder="Age"
+            onChange={(event) => handleInput(event)}
+            className="form-control"
+          />
+          <br />
+          <input
+            className="form-control"
+            type="file"
+            onChange={(event) => {
+              setFile(event.target.files[0]);
+            }}
+          />
+        </div>
+        <br />
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={handleUpload}
+        >
+          Add Data
+        </button>
+        <br />
+      </div>
+    </div>
   );
 }
 
